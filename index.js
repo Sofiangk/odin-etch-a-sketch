4d3bda5e47bea863892374b8b15649bb8b2ff46a
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-08-30 17:47:45 +0200

    Re-did the entire game

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 let color = "black";
 
-function createBoard(size) {
-  let board = document.querySelector(".board");
-  let squares = board.querySelectorAll("div");
-  squares.forEach((div) => div.remove());
-  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+function createGrid(size) {
+  let grid = document.querySelector(".grid");
+  let cells = grid.querySelectorAll("div");
+  cells.forEach((div) => div.remove());
+  grid.style.display = "grid";
+  grid.style.gap = 0;
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
-    let square = document.createElement("div");
-    square.addEventListener("mouseover", colorSquare());
-    square.style.backgroundColor = "blue";
-    square.style.border = "1px solid black";
-    board.insertAdjacentElement("beforeend", square);
-  }
-  console.log("hello");
-}
-function changeSize(input) {
-  if (input >= 2 && input <= 100) {
-    createBoard(input);
-  } else {
-    console.log("Wrong Entry!");
+    let cell = document.createElement("div");
+    cell.addEventListener("mouseover", colorCell);
+    cell.style.backgroundColor = "white";
+    cell.classList.add("cell");
+    grid.insertAdjacentElement("beforeend", cell);
   }
 }
 
-function colorSquare() {
-  if (color === "random") {
+createGrid(16);
+
+function colorCell() {
+  if (color === "rainbow") {
     this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
   } else {
     this.style.backgroundColor = color;
   }
 }
 
+function changeSize(size) {
+  if (size > 2 && size < 100) {
+    alert.textContent = "";
+    createGrid(size);
+  } else {
+    let alert = document.querySelector("#alert");
+    alert.textContent = "Please enter a number between 2 and 100";
+  }
+  createGrid(size);
+}
+
 function changeColor(choice) {
   color = choice;
 }
 
-function resetBoard() {
-  let board = document.querySelector(".board");
-  let squares = board.querySelectorAll("div");
-  squares.forEach((div) => (div.style.backgroundColor = "white"));
+function resetGrid() {
+  let grid = document.querySelector(".grid");
+  let cells = grid.querySelectorAll("div");
+  cells.forEach((div) => (div.style.backgroundColor = "white"));
 }
